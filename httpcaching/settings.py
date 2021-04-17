cdcc2c16328239ea3d17ea966263cffcfa48c353
Eralp Bayraktar <imeralpb@gmail.com>
2021-04-17 18:00:01 +0200

    Update Allowed hosts.

--- httpcaching/settings.py
+++ httpcaching/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hj22o(5b&qij2mrz-7fk*!(qq5ltssx%=mlnujnv7@wcv*^-(b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
